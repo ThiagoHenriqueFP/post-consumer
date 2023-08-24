2a870bf870771ffba004f468c8fb00dcc5a5d606
Thiago Henrique <thiagohp070@gmail.com>
2023-08-24 11:16:34 -0300

    feat: custom error handler

--- src/main/java/uol/compass/postconsumer/application/dto/ErrorHandlerDTO.java
+++ src/main/java/uol/compass/postconsumer/application/dto/ErrorHandlerDTO.java
@@ -1,2 +1,13 @@
-package uol.compass.postconsumer.application.dto;public class ErrorHandlerDTO {
+package uol.compass.postconsumer.application.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ErrorHandlerDTO(
+        LocalDateTime timestamp,
+        List<String> data
+) {
+    public ErrorHandlerDTO(List<String> data) {
+        this(LocalDateTime.now(), data);
+    }
 }
--- src/main/java/uol/compass/postconsumer/infrastructure/errorHandling/ControllerAdvice.java
+++ src/main/java/uol/compass/postconsumer/infrastructure/errorHandling/ControllerAdvice.java
@@ -1,2 +1,20 @@
-package uol.compass.postconsumer.infrastructure.errorHandling;public class ControllerAdvice {
+package uol.compass.postconsumer.infrastructure.errorHandling;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+import uol.compass.postconsumer.application.dto.ErrorHandlerDTO;
+
+import java.util.List;
+import java.util.Objects;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(ResponseStatusException.class)
+    protected ResponseEntity<Object> handlerResponseStatusException(
+            ResponseStatusException ex
+    ){
+        ErrorHandlerDTO response = new ErrorHandlerDTO(List.of(Objects.requireNonNull(ex.getReason())));
+        return new ResponseEntity<>(response, ex.getStatusCode());
+    }
 }
